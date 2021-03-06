5095a7fdf8f6e0e10b0839e7ba60293e0ef72493
Ben <benpham_12@yahoo.com>
2021-03-05 17:55:03 -0800

    Finished part 3

--- turnin/bpham028_lab13_part2.c
+++ turnin/bpham028_lab13_part2.c
@@ -29,31 +29,6 @@ void ADC_init() {
 			// Free Running Mode, a new conversion will trigger whenever the previous conversion completes
 }
 
-void transmit_data(unsigned char data, unsigned char shiftNum) {
-	int i;
-	for (i = 0; i < 8; ++i) {
-		//Sets SRCLR to 1 allowing data to be set
-		//Also clears SRCLK in preparation of sending data (shift1 SRCLR = 0x08 shift2 SRCLR = 0x20)
-		if (shiftNum == shiftC) {
-			PORTC = 0x08;
-		} else if (shiftNum == shiftD) {
-			PORTC = 0x20;
-		}
-		//set SER = next bit of data to be sent.
-		PORTC |= ((data >> i) & 0x01);
-		//set SRCLK = 1. Rising edge shifts next bit of data into the shift register
-		PORTC |= 0x02;
-	}
-	//set RCLK = 1. Rising edge copies data from "Shift" register to "Storage" register
-	if (shiftNum == shiftC) {
-		PORTC |= 0x04;
-	} else if (shiftNum == shiftD) {
-		PORTC |= 0x10;
-	}
-	//clears all lines in preparation of a new transmission
-	PORTC = 0x00;
-}
-
 enum states {start, init, wait, left, right} state;
 void Tick() {
 
